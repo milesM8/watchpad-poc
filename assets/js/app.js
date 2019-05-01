6f3faed8092a04907c79382f87cd6033f5a106a5
Robert Holden <robert@robhold.com>
2019-05-01 11:05:12 -0400

    added api keys for our main api

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    const TMDB_KEY = "d0b28beed08738ad2e0d147556889274" // https://developers.themoviedb.org/3/
+    const FANDANGO_KEY = "ug4pcrn8cz2n5eb3jpgfh425" // https://developer.fandango.com/docs
+    const TVDB_KEY = "KELUL9S1XY9AR8ZN" // https://api.thetvdb.com/swagger
+
+})
