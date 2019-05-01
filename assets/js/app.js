b12b376dcc0ba583375fbd7419f6a8402a931210
Robert Holden <robert@robhold.com>
2019-05-01 14:30:00 -0400

    moved constants over to resources.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,9 @@
 $(document).ready(function () {
-    const TMDB_KEY = "d0b28beed08738ad2e0d147556889274" // https://developers.themoviedb.org/3/
-    const FANDANGO_KEY = "ug4pcrn8cz2n5eb3jpgfh425" // https://developer.fandango.com/docs
-    //const TVDB_KEY = "KELUL9S1XY9AR8ZN" // https://api.thetvdb.com/swagger
 
+    // TMDB API Query call
     const tmdbQuery = (type, callBack, parameters = {}) => {
         let queryURL = "https://api.themoviedb.org/3"
-        const requestData = { api_key: TMDB_KEY }
+        const requestData = { api_key: strings.TMDB_KEY }
 
         switch (type) {
             case "discover":
