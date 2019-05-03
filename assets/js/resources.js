ff1c84075c4cfb220504d51e83a8024dcf5dfaba
Robert Holden <robert@robhold.com>
2019-05-03 13:48:44 -0400

    added secret

--- assets/js/resources.js
+++ assets/js/resources.js
@@ -7,5 +7,6 @@ const strings = {
     TMDB_IMAGE_URL: "https://image.tmdb.org/t/p/w342",
     TMDB_KEY: "d0b28beed08738ad2e0d147556889274", // https://developers.themoviedb.org/3/
     FANDANGO_KEY: "ug4pcrn8cz2n5eb3jpgfh425", // https://developer.fandango.com/docs
+    FANDANGO_SECRET: "Qquwh6mCG3", // https://developer.fandango.com/docs
     TVDB_KEY: "KELUL9S1XY9AR8ZN" // https://api.thetvdb.com/swagger
 }
