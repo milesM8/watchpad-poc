fc0e822da05c6dfade61ea406250b8ebec306c3a
Miles <miles.moscara@gmail.com>
2019-05-03 13:49:10 -0400

    it works

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,156 +1,186 @@
-$(document).ready(function () {
+$(document).ready(function() {
+	// adds to the local storage list
+	const localStorageAdd = (type, data) => {
+		switch (type) {
+			case "toCollection":
+				const currentCollection = localStorage.getItem("collection") ? JSON.parse(localStorage.getItem("collection")) : [];
+				let match = currentCollection.find(obj => {
+					return obj.name === data.name;
+				});
+				if (match) return false;
+				currentCollection.push(data);
+				localStorage.setItem("collection", JSON.stringify(currentCollection));
+				return JSON.parse(localStorage.getItem("collection"));
+				break;
+			case "toWatchList":
+				const currentWatchList = localStorage.getItem("watchList") ? JSON.parse(localStorage.getItem("watchList")) : [];
+				let match2 = currentWatchList.find(obj => {
+					return obj.name === data.name;
+				});
+				if (match2) return false;
+				currentWatchList.push(data);
+				localStorage.setItem("watchList", JSON.stringify(currentWatchList));
+				return JSON.parse(localStorage.getItem("watchList"));
+				break;
+			case "toIgnore":
+				const currentIgnoreList = localStorage.getItem("ignoreList") ? JSON.parse(localStorage.getItem("ignoreList")) : [];
+				let match3 = currentIgnoreList.find(obj => {
+					return obj.name === data.name;
+				});
+				if (match3) return false;
+				currentIgnoreList.push(data);
+				localStorage.setItem("ignoreList", JSON.stringify(currentIgnoreList));
+				return JSON.parse(localStorage.getItem("ignoreList"));
+				break;
+			default:
+				return false;
+				break;
+		}
+	};
 
-    // adds to the local storage list
-    const localStorageAdd = (type, data) => {
-        switch (type) {
-            case "toCollection":
-                const currentCollection = localStorage.getItem("collection") || {}
-                localStorage.setItem("collection", { ...currentCollection, data })
-                return localStorage.getItem("collection")
-                break;
-            case "toWatchList":
-                const currentWatchList = localStorage.getItem("watchList") || {}
-                localStorage.setItem("watchList", { ...currentCollection, data })
-                return localStorage.getItem("watchList")
-                break;
-            case "toIgnore":
-                const currentIgnoreList = localStorage.getItem("ignoreList") || {}
-                localStorage.setItem("ignoreList", { ...currentCollection, data })
-                return localStorage.getItem("ignoreList")
-                break;
-            default:
-                return false
-                break;
-        }
-    }
+	// gets currently stored lists
+	const localStorageGet = type => {
+		switch (type) {
+			case "collection":
+				return localStorage.getItem("collection") ? JSON.parse(localStorage.getItem("collection")) : [];
+				break;
+			case "watchList":
+				return localStorage.getItem("watchList") ? JSON.parse(localStorage.getItem("watchList")) : [];
+				break;
+			case "ignore":
+				return localStorage.getItem("ignoreList") ? JSON.parse(localStorage.getItem("ignoreList")) : [];
+				break;
+			default:
+				return false;
+				break;
+		}
+	};
 
-    // gets currently stored lists
-    const localStorageGet = (type) => {
-        switch (type) {
-            case "collection":
-                return localStorage.getItem("collection") || {}
-                break;
-            case "watchList":
-                return localStorage.getItem("watchList") || {}
-                break;
-            case "ignore":
-                return localStorage.getItem("ignoreList") || {}
-                break;
-            default:
-                return false
-                break;
-        }
-    }
+	// TMDB API Query call
+	const tmdbQuery = (type, parameters = {}) => {
+		let queryURL = strings.TMDB_URL;
+		const requestData = { api_key: strings.TMDB_KEY };
 
-    // TMDB API Query call
-    const tmdbQuery = (type, parameters = {}) => {
-        let queryURL = strings.TMDB_URL
-        const requestData = { api_key: strings.TMDB_KEY }
+		switch (type) {
+			case "discoverMovie":
+				queryURL += "/discover/movie";
+				break;
+			case "discoverTV":
+				queryURL += "/discover/tv";
+				break;
+			case "movie":
+				queryURL += `/movie/${parameters.movie_id}`;
+				break;
+			case "tv":
+				queryURL += `/tv/${parameters.tv_id}`;
+				break;
+			case "tvSeason":
+				queryURL += `/tv/${parameters.tv_id}/season/${parameters.season_number}`;
+				break;
+			case "tvEpisode":
+				queryURL += `/tv/${parameters.tv_id}/season/${parameters.season_number}/episode/${parameters.episode_number}`;
+				break;
+			case "company":
+				queryURL += `/search/company`;
+				break;
+			case "search":
+				queryURL += `/search/multi`;
+				break;
+			default:
+				return;
+		}
 
-        switch (type) {
-            case "discoverMovie":
-                queryURL += "/discover/movie"
-                break;
-            case "discoverTV":
-                queryURL += "/discover/tv"
-                break;
-            case "movie":
-                queryURL += `/movie/${parameters.movie_id}`
-                break;
-            case "tv":
-                queryURL += `/tv/${parameters.tv_id}`
-                break;
-            case "tvSeason":
-                queryURL += `/tv/${parameters.tv_id}/season/${parameters.season_number}`
-                break;
-            case "tvEpisode":
-                queryURL += `/tv/${parameters.tv_id}/season/${parameters.season_number}/episode/${parameters.episode_number}`
-                break;
-            case "company":
-                queryURL += `/search/company`
-                break;
-            case "search":
-                queryURL += `/search/multi`
-                break;
-        }
+		return (query = $.ajax({
+			url: queryURL,
+			type: "GET",
+			data: { ...requestData, ...parameters },
+			dataType: "json"
+		}));
+	};
 
-        return query = $.ajax({
-            url: queryURL,
-            type: 'GET',
-            data: { ...requestData, ...parameters },
-            dataType: 'json'
-        });
-    }
+	//renders a single poster
+	const renderPoster = media => {
+		const posterContainer = $("<div>").addClass("poster");
 
-    //renders a single poster
-    const renderPoster = media => {
-        const posterContainer = $("<div>").addClass("poster")
+		const posterImageBackdrop = $("<div>").addClass("imgBackdrop");
+		const posterImage = $("<img>").attr("src", `${strings.TMDB_IMAGE_URL}${media.poster_path}`);
+		posterImageBackdrop.append(posterImage);
 
-        const posterImageBackdrop = $("<div>").addClass("imgBackdrop")
-        const posterImage = $("<img>").attr("src", `${strings.TMDB_IMAGE_URL}${media.poster_path}`)
-        posterImageBackdrop.append(posterImage)
+		const posterBody = $("<div>").addClass("posterBody");
+		const posterTitle = $("<h3>")
+			.text(media.title)
+			.addClass("posterTitle");
+		const posterButtons = $("<div>").addClass("posterButtons");
+		const collectionButton = $("<button>")
+			.addClass("addToCollection")
+			.attr("data-name", media.title)
+			.attr("data-id", media.id)
+			.text(strings.COLLECTION);
+		const watchListButton = $("<button>")
+			.addClass("addToWatchList")
+			.attr("data-name", media.title)
+			.attr("data-id", media.id)
+			.text(strings.WATCHLIST);
+		const ignoreButton = $("<button>")
+			.addClass("addToIgnore")
+			.attr("data-name", media.title)
+			.attr("data-id", media.id)
+			.text(strings.IGNORE);
 
-        const posterBody = $("<div>").addClass("posterBody")
-        const posterTitle = $("<h3>").text(media.title).addClass("posterTitle")
-        const posterButtons = $("<div>").addClass("posterButtons")
-        const collectionButton = $("<button>")
-            .addClass("addToCollection")
-            .attr("data-name", media.title)
-            .attr("data-id", media.id)
-            .text(strings.COLLECTION)
-        const watchListButton = $("<button>")
-            .addClass("addToWatchList")
-            .attr("data-name", media.title)
-            .attr("data-id", media.id)
-            .text(strings.WATCHLIST)
-        const ignoreButton = $("<button>")
-            .addClass("addToIgnore")
-            .attr("data-name", media.title)
-            .attr("data-id", media.id)
-            .text(strings.IGNORE)
+		posterButtons.append(collectionButton, watchListButton, ignoreButton);
+		posterBody.append(posterTitle, posterButtons);
+		posterContainer.append(posterImageBackdrop, posterBody);
 
-        
+		return posterContainer;
+	};
 
-        posterButtons.append(collectionButton, watchListButton, ignoreButton)
-        posterBody.append(posterTitle, posterButtons)
-        posterContainer.append(posterImageBackdrop, posterBody)
+	// takes a search query and adds the result to a given container
+	const search = (query, container, page = 1) => {
+		container = $(container);
+		container.empty();
+		tmdbQuery("search", { query: query, page: page }).then(function(response) {
+			for (media of response.results) {
+				container.append(renderPoster(media));
+			}
+		});
+	};
 
-        return posterContainer
-    }
+	// renders the trending movies to a given container
+	const discover = (type, container, page = 1) => {
+		container = $(container);
+		container.empty();
+		tmdbQuery(`discover${type}`, { page: page }).then(function(response) {
+			for (media of response.results) {
+				container.append(renderPoster(media));
+			}
+		});
+	};
 
-    // takes a search query and adds the result to a given container
-    const search = (query, container, page = 1) => {
-        container = $(container)
-        container.empty();
-        tmdbQuery("search", { query: query, page: page }).then(function (response) {
-            for (media of response.results) {
-                container.append(renderPoster(media))
-            }
-        })
-    }
+	$("#toggleSidebar").click(function(e) {
+		const main = $("#main-section");
+		if (main.attr("data-state") === "open") {
+			main.attr("data-state", "closed");
+			main.removeClass("open");
+			main.addClass("closed");
+		} else {
+			main.attr("data-state", "open");
+			main.removeClass("closed");
+			main.addClass("open");
+		}
+	});
 
-    // renders the trending movies to a given container
-    const discover = (type, container, page = 1) => {
-        container = $(container)
-        container.empty();
-        tmdbQuery(`discover${type}`, { page: page }).then(function (response) {
-            for (media of response.results) {
-                container.append(renderPoster(media))
-            }
-        })
-    }
+	discover("Movie", "#discoverCarousel");
 
-    $("#toggleSidebar").click(function (e) {
-        const main = $("#main-section")
-        if (main.attr("data-state") === "open") {
-            main.attr("data-state", "closed")
-            main.removeClass("open")
-            main.addClass("closed")
-        }else{
-            main.attr("data-state", "open")
-            main.removeClass("closed")
-            main.addClass("open")
-        }
-    })
-    discover("Movie", "#discoverCarousel")
-})
+	$(document).on("click", ".addToWatchList", function() {
+		const button = $(this);
+		localStorageAdd("toWatchList", { name: button.attr("data-name"), id: button.attr("data-id"), date: new Date() });
+	});
+	$(document).on("click", ".addToCollection", function() {
+		const button = $(this);
+		localStorageAdd("toCollection", { name: button.attr("data-name"), id: button.attr("data-id"), date: new Date() });
+	});
+	$(document).on("click", ".addToIgnore", function() {
+		const button = $(this);
+		localStorageAdd("toIgnore", { name: button.attr("data-name"), id: button.attr("data-id"), date: new Date() });
+	});
+});
