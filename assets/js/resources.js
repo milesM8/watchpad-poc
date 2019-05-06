7e76bd40b117f4ca84076757e5ccf2de9bf27d55
Robert Holden <robert@robhold.com>
2019-05-06 14:13:41 -0400

    fixed utelly key

--- assets/js/resources.js
+++ assets/js/resources.js
@@ -8,8 +8,8 @@ const strings = {
     TMDB_IMAGE_URL: "https://image.tmdb.org/t/p/w342",
     TMDB_KEY: "d0b28beed08738ad2e0d147556889274", // https://developers.themoviedb.org/3/
     FANDANGO_KEY: "ug4pcrn8cz2n5eb3jpgfh425", // https://developer.fandango.com/docs
-    UTELLY_KEY: "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com", // https://developer.fandango.com/docs
-    UTELLY_HOST: "be531b66b0msh161af4f22b8a7e5p1bc3a7jsnbd9a9fb7befa", // https://developer.fandango.com/docs
+    UTELLY_HOST: "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com", // https://developer.fandango.com/docs
+    UTELLY_KEY: "be531b66b0msh161af4f22b8a7e5p1bc3a7jsnbd9a9fb7befa", // https://developer.fandango.com/docs
     FANDANGO_SECRET: "Qquwh6mCG3", // https://developer.fandango.com/docs
     TVDB_KEY: "KELUL9S1XY9AR8ZN" // https://api.thetvdb.com/swagger
 }
